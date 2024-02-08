5de98181981900dc383ae3d7c00256ff87457cb4
Vixs101 <elijahvix695@gmail.com>
2024-02-08 21:51:43 +0100

    tried fixing the tailwind error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Logo from '../images/Logo.png'
 
 export default function Header() {
   return (
     <div>
       <header>
-        <div>
-            <img src="" alt="" />
+        <div className="border-2">
+           <img 
+            src={Logo} 
+            alt="website Logo"
+            className="w-10" />
         </div>
       </header>
     </div>
